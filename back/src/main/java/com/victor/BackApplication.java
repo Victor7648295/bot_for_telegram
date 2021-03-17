371450abaa67317dd7f3169b2dec74b5994683bf
Victor <butekk@list.ru>
2021-03-17 22:18:29 +0300

    first commit

---
+++ back/src/main/java/com/victor/BackApplication.java
@@ -0,0 +1,14 @@
+package com.victor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackApplication.class, args);
+    }
+
+}
+
---
+++ front/src/main/java/com/victor/FrontApplication.java
@@ -0,0 +1,14 @@
+package com.victor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontApplication.class, args);
+    }
+
+}
+
